2f2090c9e24c2f42f8c9a34db369dd9efaa35832
Crystal Jeanine Edwards-Flores <florescrystal36@gmail.com>
2026-06-22 18:09:26 -0700

    Update coursework files

---
+++ PE1_M3_Week3_Lecture/Week3_Demo_BitOp.py
@@ -0,0 +1,7 @@
+a = 0b1010
+b = 0b1100
+
+print(bin(a&b))
+print(bin(a|b))
+print(bin(a^b))
+print(bin(~0b11111111))
--- PE1_M3_Week3_Lecture/Week3_Demo_Logical.py
+++ PE1_M3_Week3_Lecture/Week3_Demo_Logical.py
@@ -11,9 +11,12 @@ print(True or True)
 print(True or False)
 print(False or True)
 print(False or False)
-"""
+
 
 # 1.not  2. and  3. or
 
 print(32 > 5 and 4 < 67 or 3 > 2 and not 5 < 2)
-#print(32 > 5 and 4 < 67 or 3 > 2 and not 5 < 2)
+#print(32 > 5 and 4 < 67 or 3 > 2 and True)
+#print(True and True or True and True )
+#print(True or True)
+#print(True)
